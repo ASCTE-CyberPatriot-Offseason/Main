a25285be9be71ab798f2f9eba095171155ed6754
NotMaxwell <William.mccormick@ascte.org>
2024-11-19 09:04:00 -0600

    audit_policies works

--- Scripts/Windows/MaxwellsWindowsScript/audit_policies.py
+++ Scripts/Windows/MaxwellsWindowsScript/audit_policies.py
@@ -1,22 +1,30 @@
 import subprocess
 
+policies = [
+    "Account Logon",
+    "Account Management",
+    "Detailed Tracking",
+    "DS Access",
+    "Logon/Logoff",
+    "Object Access",
+    "Policy Change",
+    "Privilege Use",
+    "System"
+]
+
+
 def configure_audit_policies():
-    audit_policies = [
-        "Audit account logon events",
-        "Audit account management",
-        "Audit directory service access",
-        "Audit logon events",
-        "Audit object access",
-        "Audit policy change",
-        "Audit privilege use",
-        "Audit process tracking",
-        "Audit system events"
-    ]
-    command_template = 'auditpol /set /subcategory:"{}" /success:enable /failure:enable'
-    for policy in audit_policies:
-        command = command_template.format(policy)
-        subprocess.run(command, shell=True, check=True)
-        print(f"{command} executed")
+    audit_policy_mapping = policies.copy()
+    command_template = 'auditpol /set /category:"{}" /success:enable /failure:enable'
+
+    for policy_name in audit_policy_mapping:
+        command = command_template.format(policy_name)
+        print(command)
+        try:
+            subprocess.run(command, shell=True, check=True)
+            print(f"{command} executed successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing {command}: {e}")
 
 if __name__ == "__main__":
     configure_audit_policies()
